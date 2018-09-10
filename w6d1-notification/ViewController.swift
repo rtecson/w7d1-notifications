aba737cbdbed5e57f254c785b50f6307b19b1ade
Roland Tecson <roland_tecson@hotmail.com>
2018-09-09 20:57:48 -0700

    Initial commit

--- w6d1-notification/ViewController.swift
+++ w6d1-notification/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] (granted: Bool, error: Error?) in
+            if granted {
+                print("granted")
+                self?.scheduleNotification()
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func scheduleNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "My Notification"
+        content.body = "This is the content of the notification."
+        content.sound = UNNotificationSound.default()
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "uniqueId", content: content, trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: { error in
+            if let _ = error {
+                print("Error in adding notification request")
+            }
+            else {
+                print("Successfully added notification request")
+            }
+        })
     }
-
-
 }
 
